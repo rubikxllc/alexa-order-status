f7f203b80d780686101d81181419e53845d6b9cd
Andy8647 <andy8647lgc@gmail.com>
2023-09-19 17:32:32 -0400

    minor fix

--- src/alexa/ErrorHandler.ts
+++ src/alexa/ErrorHandler.ts
@@ -7,7 +7,7 @@ export const AlexaErrorHandler: ErrorHandler = {
     console.log(`Error handled: ${error.message}`);
 
     return handlerInput.responseBuilder
-      .speak("Sorry, I don't understand your command. andy")
+      .speak("Sorry, I don't understand your command. Please say again!!!")
       .reprompt("Sorry, I don't understand your command.")
       .getResponse();
   },
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,8 @@
-import { SkillBuilders } from 'ask-sdk-core';
+import { RequestEnvelope } from 'ask-sdk-model';
+import { AlexaSkill } from './alexa';
 
-import { LaunchRequestHandler } from './alexa/LaunchRequestHandler';
-import { AskOrderStatusRequestHandler } from './alexa/AskOrderStatusRequestHandler';
-import { AlexaErrorHandler } from './alexa/ErrorHandler';
-import { HelpIntentHandler } from './alexa/HelpIntentHandler';
-import { CancelAndStopIntentHandler } from './alexa/CancelAndStopIntentHandler';
-import { SessionEndedRequestHandler } from './alexa/SessionEndHandler';
-
-let skill;
-
-exports.handler = async function (event, context) {
-  console.log(`REQUEST++++${JSON.stringify(event)}`);
-
-  if (!skill) {
-    skill = SkillBuilders.custom()
-      .addRequestHandlers(
-        LaunchRequestHandler,
-        AskOrderStatusRequestHandler,
-        HelpIntentHandler,
-        CancelAndStopIntentHandler,
-        SessionEndedRequestHandler,
-      )
-      .addErrorHandlers(AlexaErrorHandler)
-      .create();
-  }
-
-  const response = await skill.invoke(event, context);
-  console.log(`RESPONSE++++${JSON.stringify(response)}`);
-
-  return response;
+exports.handler = async function (event: RequestEnvelope, context: any) {
+  const skill = new AlexaSkill();
+  console.log('========= Alexa Skill ========');
+  return await skill.invoke(event, context);
 };
